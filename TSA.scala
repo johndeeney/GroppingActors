9503f54aa2cabc9378e593e9366d652aa1d023ee
Matthew Witte <mcw9612@rit.edu>
2014-12-06 00:12:27 -0500

    Compiles and runs

--- TSA.scala
+++ TSA.scala
@@ -1,37 +1,50 @@
+import akka.actor.Actor
+import akka.actor.ActorSystem
+import akka.actor.Props
+
 case class Person(name: String)
 
-class DocumentCheckActor extends Actor with ActorLogging {
-  def receive = {
-    case Greeting(name) ? log.info(who)
+class DocumentCheckActor extends Actor {
+  def receive() = {
+    case Person(name) => println(name)
   }
 }
 
-class QueueActor extends Actor with ActorLogging {
-  def receive = {
-    case Greeting(name) ? log.info(who)
+class QueueActor extends Actor {
+  def receive() = {
+    case Person(name) => println(name)
   }
 }
 
-class BagScanActor extends Actor with ActorLogging {
-  def receive = {
-    case Greeting(name) ? log.info(who)
+class BagScanActor extends Actor {
+  def receive() = {
+    case Person(name) => println(name)
   }
 }
 
-class BodyScanActor extends Actor with ActorLogging {
-  def receive = {
-    case Greeting(name) ? log.info(who)
+class BodyScanActor extends Actor {
+  def receive() = {
+    case Person(name) => println(name)
   }
 }
 
-class SecurityActor extends Actor with ActorLogging {
-  def receive = {
-    case Greeting(name) ? log.info(who)
+class SecurityActor extends Actor {
+  def receive() = {
+    case Person(name) => println(name)
   }
 }
 
-class JailActor extends Actor with ActorLogging {
-  def receive = {
-    case Greeting(name) ? log.info(who)
+class JailActor extends Actor {
+  def receive() = {
+    case Person(name) => println(name)
+  }
+}
+
+object Main {
+  def main(args: Array[String]) = {
+    val system = ActorSystem("MySystem")
+    val docCheck = system.actorOf(Props[DocumentCheckActor], name = "docCheck")
+    docCheck ! Person("Charlie Parker")
+    system.shutdown()
   }
 }

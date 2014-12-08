41b1123a05ca21c2e92dba252062aaa9448b3d99
Matthew Witte <mcw9612@rit.edu>
2014-12-08 14:30:54 -0500

    Initialize actors

--- TSA.scala
+++ TSA.scala
@@ -5,10 +5,17 @@ import akka.actor.Props
 object TSA {
   val numLines = 3
   val numPassengers = 20
+  val system = ActorSystem("MySystem")
+  var docCheck = system.actorOf(Props[DocumentCheckActor], name = "docCheck")
+  var queueArray:Array[QueueActor] = Array.empty[QueueActor]
+  var bagArray:Array[BagScanActor] = Array.empty[BagScanActor]
+  var bodyArray:Array[BodyScanActor] = Array.empty[BodyScanActor]
+  var securityArray:Array[SecurityActor] = Array.empty[SecurityActor]
+  var jail:JailActor = system.actorOf(Props[JailActor], name = "jail")
 
   case class Person(name: String)
 
-  class DocumentCheckActor(numLines: Int) extends Actor {
+  class DocumentCheckActor extends Actor {
     var lastLine = 0
     def receive() = {
       case Person(name) =>
@@ -51,8 +58,16 @@ object TSA {
   }
 
   def main(args: Array[String]) = {
-    val system = ActorSystem("MySystem")
-    val docCheck = system.actorOf(Props(new DocumentCheckActor(numLines)), name = "docCheck")
+    queueArray = new Array[QueueActor](numLines)
+    bagArray = new Array[BagScanActor](numLines)
+    bodyArray = new Array[BodyScanActor](numLines)
+    securityArray = new Array[SecurityActor](numLines)
+    for(i <- 0 to (numLines-1)){
+      queueArray(i) = system.actorOf(Props(new QueueActor(i)), name = "queue".concat(i+""))
+      bagArray(i) = system.actorOf(Props(new BagScanActor(i)), name = "bag".concat(i+""))
+      bodyArray(i) = system.actorOf(Props(new BodyScanActor(i)), name = "body".concat(i+""))
+      securityArray(i) = system.actorOf(Props(new SecurityActor(i)), name = "security".concat(i+""))
+    }
     for(a <- 1 to numPassengers){
       docCheck ! Person(a + "")
     }

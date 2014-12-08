2cfc4d01819aabb92e73fd109bccf5fb38ff6996
Matthew Witte <mcw9612@rit.edu>
2014-12-08 13:16:18 -0500

    Refactored and started document check

--- TSA.scala
+++ TSA.scala
@@ -2,49 +2,60 @@ import akka.actor.Actor
 import akka.actor.ActorSystem
 import akka.actor.Props
 
-case class Person(name: String)
+object TSA {
+  val numLines = 3
+  val numPassengers = 20
 
-class DocumentCheckActor extends Actor {
-  def receive() = {
-    case Person(name) => println(name)
+  case class Person(name: String)
+
+  class DocumentCheckActor(numLines: Int) extends Actor {
+    var lastLine = 0
+    def receive() = {
+      case Person(name) =>
+        println(name + " arrives at Document Check")
+        lastLine = lastLine + 1
+        if (lastLine > numLines) {
+          lastLine = 0
+        }
+    }
   }
-}
 
-class QueueActor extends Actor {
-  def receive() = {
-    case Person(name) => println(name)
+  class QueueActor extends Actor {
+    def receive() = {
+      case Person(name) => println(name)
+    }
   }
-}
 
-class BagScanActor extends Actor {
-  def receive() = {
-    case Person(name) => println(name)
+  class BagScanActor extends Actor {
+    def receive() = {
+      case Person(name) => println(name)
+    }
   }
-}
 
-class BodyScanActor extends Actor {
-  def receive() = {
-    case Person(name) => println(name)
+  class BodyScanActor extends Actor {
+    def receive() = {
+      case Person(name) => println(name)
+    }
   }
-}
 
-class SecurityActor extends Actor {
-  def receive() = {
-    case Person(name) => println(name)
+  class SecurityActor extends Actor {
+    def receive() = {
+      case Person(name) => println(name)
+    }
   }
-}
 
-class JailActor extends Actor {
-  def receive() = {
-    case Person(name) => println(name)
+  class JailActor extends Actor {
+    def receive() = {
+      case Person(name) => println(name)
+    }
   }
-}
 
-object Main {
   def main(args: Array[String]) = {
     val system = ActorSystem("MySystem")
-    val docCheck = system.actorOf(Props[DocumentCheckActor], name = "docCheck")
-    docCheck ! Person("Charlie Parker")
+    val docCheck = system.actorOf(Props(new DocumentCheckActor(numLines)), name = "docCheck")
+    for(a <- 1 to numPassengers){
+      docCheck ! Person(a + "")
+    }
     system.shutdown()
   }
 }
